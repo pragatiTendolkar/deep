95bbaa0a1ba3a7461c1c4d4f23351485e3a0fbaf
pragatiTendolkar <you@example.com>
2024-08-21 13:13:21 +0530

    change in file

--- src/Components/Firstp/Four.jsx
+++ src/Components/Firstp/Four.jsx
@@ -42,7 +42,7 @@ export default function Four() {
                             />
                         )}
                         
-                        <p>Published on: {new Date(blog.attributes.publishedAt).toLocaleDateString()}</p>
+                        <p>Published on the {new Date(blog.attributes.publishedAt).toLocaleDateString()}</p>
                         <h4>{blog.attributes.Name || 'No Title'}  <span> <svg width="20" height="20" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path fill-rule="evenodd" clip-rule="evenodd" d="M30.8839 9.11612C31.372 9.60427 31.372 10.3957 30.8839 10.8839L10.8839 30.8839C10.3957 31.372 9.60427 31.372 9.11612 30.8839C8.62796 30.3957 8.62796 29.6043 9.11612 29.1161L29.1161 9.11612C29.6043 8.62796 30.3957 8.62796 30.8839 9.11612Z" fill="currentColor" fill-opacity="0.9"></path>
                                         <path fill-rule="evenodd" clip-rule="evenodd" d="M12.5 10C12.5 9.30964 13.0596 8.75 13.75 8.75H30C30.6904 8.75 31.25 9.30964 31.25 10V26.25C31.25 26.9404 30.6904 27.5 30 27.5C29.3096 27.5 28.75 26.9404 28.75 26.25V11.25H13.75C13.0596 11.25 12.5 10.6904 12.5 10Z" fill="currentColor" fill-opacity="0.9"></path>
